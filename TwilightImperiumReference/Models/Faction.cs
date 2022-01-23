b120fbd935c22b0352fbb9273a122d8386889526
wpknox <wpknox@iastate.edu>
2022-01-23 13:57:24 -0700

    get faction list page to open. Now to make more list pages

--- TwilightImperiumReference/Models/Faction.cs
+++ TwilightImperiumReference/Models/Faction.cs
@@ -1,19 +1,22 @@
 ﻿namespace TwilightImperiumReference.Models;
 
-// will need to update JSON file for Planets
+// will need to update JSON file for Planets and Tech
 public class Faction
 {
     public string Id { get; set; } = "";
     public string Name { get; set; } = "";
     public string Icon { get; set; } = "";
-    public List<Technology>? StartingTechnology { get; set; }
+
+    public List<string>? StartingTechnology { get; set; }
+    //public List<Technology>? StartingTechnology { get; set; }
     public List<StartingUnit> StartingUnits { get; set; } = new();
-    public List<Planet> HomePlanets { get; set; } = new();
+    //public List<Planet> HomePlanets { get; set; } = new();
+    public List<string> HomePlanets { get; set; } = new();
     public int Commodities { get; set; }
     public List<FactionAbility>? FactionAbilities { get; set; }
     public List<FactionTechnology>? FactionTechnology { get; set; }
     public List<FactionUnit>? FactionUnits { get; set; }
-    public PromissoryNote FactionPromissoryNotes { get; set; } = new();
+    public PromissoryNote FactionPromissoryNote { get; set; } = new();
     public Flagship Flagship { get; set; } = new();
     public Mech Mech { get; set; } = new();
     public List<Leader> Leaders { get; set; } = new();

1d8249a706b41f1057b91805d96d77bf77fb8ad5
wpknox <knox.wp@gmail.com>
2022-04-15 13:53:35 -0600

    delete the example files. Improve list extension

--- TwilightImperiumReference/Extensions/Extensions.cs
+++ TwilightImperiumReference/Extensions/Extensions.cs
@@ -2,8 +2,6 @@ namespace TwilightImperiumReference;
 
 public static class ListExtensions
 {
-    public static string Join(this List<string> list, string delimiter = ", ")
-    {
-        return string.Join(delimiter, list ?? new List<string>());
-    }
+    public static string Join<T>(this List<T> list, string delimiter = ", ") =>
+        string.Join(delimiter, list ?? new List<T>());
 }

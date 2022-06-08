0ef04e221f94a9e87c2ad66d176195d247b6fbbe
wpknox <knox.wp@gmail.com>
2022-06-07 20:14:48 -0600

    use code behind for Factions

--- TwilightImperiumReference/Pages/Factions.razor.cs
+++ TwilightImperiumReference/Pages/Factions.razor.cs
@@ -3,21 +3,19 @@ using Microsoft.AspNetCore.Components;
 using TwilightImperiumReference.Models;
 
 namespace TwilightImperiumReference.Pages;
-public partial class Leaders
+
+public partial class Factions
 {
     [Inject]
     protected HttpClient Http { get; set; } = new();
 
     [Inject]
     protected NavigationManager? Navigate { get; set; }
-
-    protected List<Leader>? AllLeaders { get; set; }
-    protected string? SearchString { get; set; }
+    protected List<FactionDTO>? AllFactions { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
-        AllLeaders = await Http.GetFromJsonAsync<List<Leader>>("data/leaders.json");
+        AllFactions = await Http.GetFromJsonAsync<List<FactionDTO>>("data/factions.json");
     }
-
     protected void NavigateToFactionPage(string factionId) => Navigate!.NavigateTo($"/factions/{factionId}");
 }

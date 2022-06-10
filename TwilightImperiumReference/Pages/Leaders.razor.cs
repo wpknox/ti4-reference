0fe86c8003f0f30b0cf395933a058fe08e702af5
wpknox <knox.wp@gmail.com>
2022-06-10 01:00:03 -0600

    make search by name work on Leadrrs

--- TwilightImperiumReference/Pages/Leaders.razor.cs
+++ TwilightImperiumReference/Pages/Leaders.razor.cs
@@ -1,4 +1,5 @@
 using System.Net.Http.Json;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Components;
 using TwilightImperiumReference.Models;
 
@@ -11,13 +12,37 @@ public partial class Leaders
     [Inject]
     protected NavigationManager? Navigate { get; set; }
 
-    protected List<Leader>? AllLeaders { get; set; }
+    private List<Leader> _allLeaders = new();
+
+    protected List<Leader>? DisplayedLeaders { get; set; }
+
     protected string? SearchString { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
-        AllLeaders = await Http.GetFromJsonAsync<List<Leader>>("data/leaders.json");
+        _allLeaders = await Http.GetFromJsonAsync<List<Leader>>("data/leaders.json") ?? new();
+        DisplayedLeaders = new(_allLeaders);
     }
 
     protected void NavigateToFactionPage(string factionId) => Navigate!.NavigateTo($"/factions/{factionId}");
+
+    protected void FilterLeaders()
+    {
+        if (SearchString is null or "")
+        {
+            Reset();
+            return;
+        }
+        var searchString = SearchString.ToUpperInvariant();
+        var regex = new Regex("[^a-zA-Z]"); // ignore "-", " ' " (apostrophes), and " " from names
+        DisplayedLeaders = new(_allLeaders.Where(l => regex.Replace(l.Name, "")
+                                                           .ToUpperInvariant()
+                                                           .Contains(searchString)));
+    }
+
+    protected void Reset()
+    {
+        DisplayedLeaders = new(_allLeaders);
+        SearchString = null;
+    }
 }
